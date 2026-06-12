99fbb4a3f161a8a7ed77ba24f73d62e57da88f31
rcsurar0-crypto <rcsurar0@gmail.com>
2026-06-12 22:21:27 +0200

    Update JarvisAccessibilityService.kt

--- JarvisAccessibilityService.kt
+++ JarvisAccessibilityService.kt
@@ -1,29 +1,18 @@
+import android.accessibilityservice.AccessibilityService
+import android.view.accessibility.AccessibilityEvent
+
 class JarvisAccessibilityService : AccessibilityService() {
 
-    override fun onAccessibilityEvent(event: AccessibilityEvent?) {}
+    override fun onServiceConnected() {
+        super.onServiceConnected()
+
+        AccessibilityController.service = this
+        println("Jarvis Accessibility connected")
+    }
+
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+        // később: UI tracking / Vision input
+    }
 
     override fun onInterrupt() {}
-
-    fun click(x: Int, y: Int) {
-        val path = Path()
-        path.moveTo(x.toFloat(), y.toFloat())
-
-        val gesture = GestureDescription.Builder()
-            .addStroke(GestureDescription.StrokeDescription(path, 0, 100))
-            .build()
-
-        dispatchGesture(gesture, null, null)
-    }
-
-    fun swipe(x1: Int, y1: Int, x2: Int, y2: Int) {
-        val path = Path()
-        path.moveTo(x1.toFloat(), y1.toFloat())
-        path.lineTo(x2.toFloat(), y2.toFloat())
-
-        val gesture = GestureDescription.Builder()
-            .addStroke(GestureDescription.StrokeDescription(path, 0, 300))
-            .build()
-
-        dispatchGesture(gesture, null, null)
-    }
 }
