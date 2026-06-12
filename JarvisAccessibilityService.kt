f1ebb6aa25e55de84ea0a1bdf47a27b4ee725643
rcsurar0-crypto <rcsurar0@gmail.com>
2026-06-12 15:44:56 +0200

    Create JarvisAccessibilityService.kt

---
+++ JarvisAccessibilityService.kt
@@ -0,0 +1,29 @@
+class JarvisAccessibilityService : AccessibilityService() {
+
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {}
+
+    override fun onInterrupt() {}
+
+    fun click(x: Int, y: Int) {
+        val path = Path()
+        path.moveTo(x.toFloat(), y.toFloat())
+
+        val gesture = GestureDescription.Builder()
+            .addStroke(GestureDescription.StrokeDescription(path, 0, 100))
+            .build()
+
+        dispatchGesture(gesture, null, null)
+    }
+
+    fun swipe(x1: Int, y1: Int, x2: Int, y2: Int) {
+        val path = Path()
+        path.moveTo(x1.toFloat(), y1.toFloat())
+        path.lineTo(x2.toFloat(), y2.toFloat())
+
+        val gesture = GestureDescription.Builder()
+            .addStroke(GestureDescription.StrokeDescription(path, 0, 300))
+            .build()
+
+        dispatchGesture(gesture, null, null)
+    }
+}
